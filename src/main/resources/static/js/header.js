343362f2b712717b878ccdf2a039ce2687768c94
지토지토 <iam@zito.kr>
2025-05-23 16:30:20 +0900

    init

---
+++ src/main/resources/static/js/header.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('mobile-menu-button');
+const menu = document.getElementById('mobile-menu');
+btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+});
