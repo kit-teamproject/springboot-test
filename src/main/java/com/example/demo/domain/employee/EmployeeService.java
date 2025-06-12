7ff49696e60dcc431cb0a0043cc17c59f922269c
지토지토 <iam@zito.kr>
2025-06-13 00:27:59 +0900

    feat : dto -> entity 로 변환하여 저장, 리스트는 dto 로 변환하여 반환

--- src/main/java/com/example/demo/domain/employee/EmployeeService.java
+++ src/main/java/com/example/demo/domain/employee/EmployeeService.java
@@ -1,11 +1,16 @@
 package com.example.demo.domain.employee;
 
+import com.example.demo.domain.employee.dto.ReqEmployeeForm;
+import com.example.demo.domain.employee.dto.ResEmployeeDto;
 import lombok.RequiredArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,17 +19,38 @@ public class EmployeeService {
     private final EmployeeRepository employeeRepository;
 
     @Transactional
-    public void save(EmployeeEntity employee) {
-        employeeRepository.save(employee);
+    public void save(ReqEmployeeForm reqEmployeeForm) {
+        //비밀번호 암호화
+        String passwordEncoded = BCrypt.hashpw(reqEmployeeForm.getPassword(), BCrypt.gensalt());
+
+        EmployeeEntity employeeEntity = EmployeeEntity.builder()
+                .userid(reqEmployeeForm.getUserid())
+                .password(passwordEncoded)
+                .phone(reqEmployeeForm.getPhone())
+                .name(reqEmployeeForm.getName())
+                .address(reqEmployeeForm.getAddress())
+                .role(reqEmployeeForm.getRole())
+                .department(reqEmployeeForm.getDepartment())
+                .sms_receive(reqEmployeeForm.getSms_receive())
+                .build();
+        employeeRepository.save(employeeEntity);
     }
 
     @Transactional(readOnly = true)
-    public List<EmployeeEntity> getAllEmployees() {
-        return employeeRepository.findAll();
+    public List<ResEmployeeDto> getAllEmployees() {
+        List<ResEmployeeDto> result = employeeRepository.findAll()
+                .stream()
+                .map(ResEmployeeDto::employeeEntityToDto)
+                .collect(Collectors.toList());
+
+        return result;
     }
 
     @Transactional(readOnly = true)
-    public Optional<EmployeeEntity> findByUserid(String userid) {
-        return employeeRepository.findByUserid(userid);
+    public ResEmployeeDto findByUserid(String userid) {
+        ResEmployeeDto result = employeeRepository.findByUserid(userid)
+                .map(ResEmployeeDto::employeeEntityToDto)
+                .orElse(null);
+        return result;
     }
 }
