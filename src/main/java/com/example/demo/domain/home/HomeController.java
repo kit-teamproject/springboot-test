e499662a21e5deaf15fe0605efa2933759706d6a
지토지토 <iam@zito.kr>
2025-06-18 00:45:25 +0900

    feat : 스토리지 사용 테스트

--- src/main/java/com/example/demo/domain/home/HomeController.java
+++ src/main/java/com/example/demo/domain/home/HomeController.java
@@ -1,13 +1,29 @@
 package com.example.demo.domain.home;
 
+import com.example.demo.domain.employee.EmployeeRepository;
+import com.example.demo.domain.employee.dto.ResEmployeeDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final EmployeeRepository employeeRepository;
+
     //    메인페이지
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<ResEmployeeDto> employees = employeeRepository.findAll()
+                .stream()
+                .map(ResEmployeeDto::employeeEntityToDto)
+                .toList();
+
+        model.addAttribute("employees", employees);
+
         return "home";
     }
 }
